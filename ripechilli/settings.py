591fd7b75a038b4b5dd46e9318498f3ddc7f7262
Anas Nadeem <57933598+AnasNadeem@users.noreply.github.com>
2021-09-28 11:34:24 +0530

    Update settings.py

--- ripechilli/settings.py
+++ ripechilli/settings.py
@@ -131,8 +131,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Email settings 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'ripechilli@gmail.com'
-EMAIL_HOST_PASSWORD = 'uwqfvknudsyjujvd'
+EMAIL_HOST_USER = 'xyz@gmail.com'
+EMAIL_HOST_PASSWORD = 'xyz'
 #uwqfvknudsyjujvd
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False
