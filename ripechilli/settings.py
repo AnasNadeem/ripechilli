7c05957e98fd8319d3c93ad4fff34c1c8606ddf3
Anas Nadeem <57933598+AnasNadeem@users.noreply.github.com>
2021-10-14 15:00:28 +0530

    Update settings.py

--- ripechilli/settings.py
+++ ripechilli/settings.py
@@ -133,6 +133,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'xyz@gmail.com'
 EMAIL_HOST_PASSWORD = 'xyz'
-#uwqfvknudsyjujvd
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False
